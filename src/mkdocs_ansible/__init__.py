7e7fda1a34776a201fbd475db04abfb18515d35e
Sorin Sbarnea <ssbarnea@redhat.com>
2023-03-14 17:03:18 +0000

    Add extra dependencies (#4)

--- src/mkdocs_ansible/__init__.py
+++ src/mkdocs_ansible/__init__.py
@@ -1,6 +1,6 @@
 """mkdocs-ansible theme."""
 try:
-    from ._version import version as __version__
+    from ._version import version as __version__  # type: ignore
 except ImportError:  # pragma: no cover
     __version__ = "0.1.dev1"
 
